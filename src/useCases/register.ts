7ea35f492c410d71e3def3f804b9b0c029cf71fa
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-06-05 22:38:56 +0100

    refactor: Type return of registerUseCase

--- src/useCases/register.ts
+++ src/useCases/register.ts
@@ -1,4 +1,5 @@
 import { hash } from "bcryptjs";
+import { User } from "@prisma/client";
 
 import { IUsersRepository } from "@/repositories/interfaces/IUsersRepository";
 import { UserAlreadyExistError } from "./error/userAlreadyExistError";
@@ -9,10 +10,18 @@ interface IRegisterUseCaseDTO {
   password: string;
 }
 
+interface IRegisterUseCaseResponse {
+  user: User;
+}
+
 class RegisterUseCase {
   constructor(private usersRepository: IUsersRepository) { }
 
-  async execute({ name, email, password }: IRegisterUseCaseDTO) {
+  async execute({
+    name,
+    email,
+    password,
+  }: IRegisterUseCaseDTO): Promise<IRegisterUseCaseResponse> {
     const emailAlreadyRegister = await this.usersRepository.findByEmail(email);
 
     if (emailAlreadyRegister) {
@@ -21,11 +30,15 @@ class RegisterUseCase {
 
     const password_hash = await hash(password, 6);
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return {
+      user,
+    };
   }
 }
 
