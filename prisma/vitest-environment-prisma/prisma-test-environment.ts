4f52210c810c9b9eedd65fec023d56003832eefb
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-07-21 20:11:18 +0100

    test(e2e): setup environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,41 @@
+import "dotenv/config";
+
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "node:child_process";
+import { randomUUID as uuid } from "node:crypto";
 import { Environment } from "vitest";
 
-export default <Environment> {
+const prisma = new PrismaClient();
+
+function genareteDatabaseUrl(schema: string): string {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Provide a DATABASE_URL in the environment variables");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
+
+export default <Environment>{
   name: "prisma",
   async setup() {
-    console.log("Executed");
+    const schema = uuid();
 
+    const databaseUrl = genareteDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync("npx prisma migrate deploy");
     return {
-      teardown(): void {
-        console.log("After execution");
+      async teardown() {
+        await prisma.$queryRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        );
+
+        prisma.$disconnect();
       },
     };
-  }
+  },
 };
