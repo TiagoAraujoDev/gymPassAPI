1430740878566d322b7149a6e0331952314844f5
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2023-07-24 15:25:19 +0100

    fix(controllers): receiving properties from the query params

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { makeFetchNearbyGymsUseCase } from "@/useCases/factories/makeFetchNearbyGymsUseCase";
 
 async function nearby(request: FastifyRequest, reply: FastifyReply) {
-  const fetchNearbyGymsBodySchema = z.object({
+  const fetchNearbyGymsQuerySchema = z.object({
     latitude: z.number().refine((value) => {
       return Math.abs(value) <= 90;
     }),
@@ -13,7 +13,7 @@ async function nearby(request: FastifyRequest, reply: FastifyReply) {
     }),
   });
 
-  const { longitude, latitude } = fetchNearbyGymsBodySchema.parse(request.body);
+  const { longitude, latitude } = fetchNearbyGymsQuerySchema.parse(request.query);
 
   const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase();
 
